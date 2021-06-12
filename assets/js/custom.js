67e8d5506ae9b6cd58e4ccb05190cf68e618b6e6
nevikw39 <nevik.w39@gmail.com>
2021-06-12 15:59:29 +0800

    Hide Disqus Ads!!

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,1 @@
-document.querySelectorAll("iframe").forEach(i => { if (i.src.includes("ads")) i.remove() })
+document.addEventListener('DOMContentLoaded', () => document.querySelectorAll("iframe").forEach(i => { if (i.src.includes("ads")) i.remove() }))
