a84aaf85b73e99826d0a2afabb50102dfe792197
nevikw39 <nevik.w39@gmail.com>
2021-08-01 02:21:08 +0800

    Hide annoying Disqus ads

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,12 @@
-// document.addEventListener('DOMContentLoaded', () => document.querySelectorAll("iframe").forEach(i => { if (i.src.includes("ads")) i.remove() }))
+(function () {
+    "use strict";
+    new Promise(r => {
+        const id = setInterval(() => {
+            const disqus = document.getElementById("disqus_thread");
+            if (disqus.childElementCount > 0) {
+                clearInterval(id);
+                r(disqus);
+            }
+        }, 87);
+    }).then(disqus => disqus.children[0].style.display = 'none');
+})();
